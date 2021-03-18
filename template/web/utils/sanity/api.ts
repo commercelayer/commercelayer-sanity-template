ca9a044d397e504661c0e9f627383a50efb280bc
Alessandro Casazza <alessandro@commercelayer.io>
2021-03-18 16:41:45 +0100

    chore: Change Sanity init variables

--- template/web/utils/sanity/api.ts
+++ template/web/utils/sanity/api.ts
@@ -11,8 +11,8 @@ import {
 } from './typings'
 
 const client = sanityClient({
-  projectId: process.env.projectId as string,
-  dataset: process.env.dataset as string,
+  projectId: process.env.SANITY_PROJECT_ID as string,
+  dataset: process.env.SANITY_DATASET as string,
   token: process.env.SANITY_TOKEN as string, // or leave blank to be anonymous user
   useCdn: process.env.NODE_ENV === 'production', // `false` if you want to ensure fresh data
 })
