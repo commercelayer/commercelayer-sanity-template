cf67946c9775ea69ddf6dcbe2791dde16cf2fd42
Bolaji Ayodeji <sirbeejay1@gmail.com>
2023-07-10 11:34:19 +0100

    chore: update sanity packages

--- utils/sanity/api.ts
+++ utils/sanity/api.ts
@@ -14,7 +14,7 @@ const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION as string,
-  useCdn: process.env.NODE_ENV === "production" // `false` if you want to ensure fresh data
+  useCdn: process.env.NODE_ENV === "production" // `false` to ensure fresh data
 });
 
 const allCountries = async (locale = "en-US") => {
