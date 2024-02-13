ce71df546c491c5d675af803f11e24eb73ee5cf0
alexcanessa <canessa.alex@gmail.com>
2024-02-13 13:37:18 +0100

    build: resolve vulnerabilities
    BREAKING CHANGE: Update several packages to latest major.
    
    Signed-off-by: alexcanessa <canessa.alex@gmail.com>

--- pages/studio/[[...index]].tsx
+++ pages/studio/[[...index]].tsx
@@ -1,16 +1,13 @@
-import Head from "next/head";
+// import Head from "next/head";
 import { NextStudio } from "next-sanity/studio";
-import { NextStudioHead } from "next-sanity/studio/head";
 import { StudioLayout, StudioProvider } from "sanity";
 import config from "../../sanity.config";
+
+export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
   return (
     <>
-      <Head>
-        <NextStudioHead favicons={false} />
-      </Head>
-
       <NextStudio config={config}>
         <StudioProvider config={config}>
           <StudioLayout />
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
+import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import schemas from "./schemas/schemaTypes";
 import { Logo } from "./plugins/studioLogo";
 
-const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || "[Studio] Commerce Layer Sanity Starter";
+const title =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || "[Studio] Commerce Layer Sanity Starter";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
@@ -14,7 +15,7 @@ export default defineConfig({
   basePath: "/studio",
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool({ defaultApiVersion: apiVersion })],
+  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
   schema: {
     types: schemas
   },
